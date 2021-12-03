4d85845aea5ec57dd386ddb255f6810f5f074311
Ilya Vojjov <voggov1@mail.ru>
2021-12-03 09:04:28 +0300

    new commit

--- Lab6/src/Main.java
+++ Lab6/src/Main.java
@@ -6,13 +6,17 @@ import java.net.http.HttpResponse;
 public class Main {
     public static void main(String[] args) {
         WeatherClass weatherClass = new WeatherClass();
-        String str = weatherClass.getURL("http://api.openweathermap.org/data/2.5/weather?q="+ "Пенза" + "&appid=1677386401e9c6ee40d319dfa9b9686a");
+        String city = "Пенза";
+        String str = weatherClass.getURL("http://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=1677386401e9c6ee40d319dfa9b9686a");
         System.out.println(str);
         if (!str.isEmpty()){
             JSONObject obj = new JSONObject(str);
+            System.out.println("Город: " + city);
             System.out.println("Температура: " + obj.getJSONObject("main").getDouble("temp"));
-            System.out.println("Температура: " + obj.getJSONObject("main").getDouble("feels_like"));
-            System.out.println("Температура: " + obj.getJSONObject("main").getDouble("temp"));
+            System.out.println("Ощущается: " + obj.getJSONObject("main").getDouble("feels_like"));
+            System.out.println("Скорость ветра: " + obj.getJSONObject("wind").getDouble("speed"));
+
+
         }
     }
 }
